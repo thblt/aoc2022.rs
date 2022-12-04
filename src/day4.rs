46a0a0f9caec006a7a0c9c98187b0971679a53fd
Thibault Polge <thibault@thb.lt>
2022-12-04 10:28:15 +0100

    Simplify

--- src/day4.rs
+++ src/day4.rs
@@ -20,27 +20,17 @@ fn read_input(path: &str) -> Vec<InputLine> {
     ret
 }
 
-fn part1() {
-    let mut count = 0;
-    for ((a1,a2),(b1,b2)) in read_input("inputs/4.txt") {
-        if (a1 >= b1 && a2 <= b2) ||  (a1 <= b1 && a2 >= b2){
-            count += 1;
-        }
-    }
-    println!("Part 1: {}", count);
-}
-
-fn part2() {
-    let mut count = 0;
-    for ((a1,a2),(b1,b2)) in read_input("inputs/4.txt") {
-        if !((a1 < b1 && a2 < b1) || (a1 > b1 && a1 > b2)) {
-            count += 1;
-        }
-    }
-    println!("Part 2: {}", count);
-}
-
 fn main() {
-    part1();
-    part2();
+    let mut count1 = 0;
+    let mut count2 = 0;
+    for ((a1,a2),(b1,b2)) in read_input("inputs/4.txt") {
+        if (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2){
+            count1 += 1;
+        }
+        if !((a1 < b1 && a2 < b1) || (a1 > b1 && a1 > b2)) {
+            count2 += 1;
+        }
+    }
+    println!("Part 1: {}", count1);
+    println!("Part 2: {}", count2);
 }
