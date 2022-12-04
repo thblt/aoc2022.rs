2d21e0423569640c57ee7e86aeec5b103525d628
Thibault Polge <thibault@thb.lt>
2022-12-04 22:27:33 +0100

    Use sscanf instead on hand parsing on day4

--- src/day4.rs
+++ src/day4.rs
@@ -1,21 +1,13 @@
 use lib::*;
+use sscanf::sscanf;
 
-type InputLine = ((u32, u32),(u32, u32));
+type InputLine = (u32, u32,u32, u32);
 
 fn read_input(path: &str) -> Vec<InputLine> {
-    fn read_pair(s: &str) -> (u32, u32) {
-        let mut halves = s.split('-');
-        (halves.next().unwrap().parse::<u32>().unwrap(),
-         halves.next().unwrap().parse::<u32>().unwrap())
-    }
-
     let mut ret: Vec<InputLine> = vec!();
-
     for line in read_lines(path) {
         let line = line.unwrap();
-        let mut halves= line.split(',');
-        ret.push((read_pair(halves.next().unwrap()),
-                  read_pair(halves.next().unwrap())));
+        ret.push(sscanf!(line, "{u32}-{u32},{u32}-{u32}").unwrap());
     }
     ret
 }
@@ -23,7 +15,7 @@ fn read_input(path: &str) -> Vec<InputLine> {
 fn main() {
     let mut count1 = 0;
     let mut count2 = 0;
-    for ((a1,a2),(b1,b2)) in read_input("inputs/4.txt") {
+    for (a1,a2,b1,b2) in read_input("inputs/4.txt") {
         if (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2){
             count1 += 1;
         }
