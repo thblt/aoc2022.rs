bfe47d1abbcb035c6c9b09fb94bea2dfd10e72cc
Thibault Polge <thibault@thb.lt>
2022-12-05 10:47:17 +0100

    Simplify day 5

--- src/day5.rs
+++ src/day5.rs
@@ -12,8 +12,7 @@ type Stacks = Vec<Stack>;
 type Instructions = Vec<Instruction>;
 
 fn read_input(s: &str) -> (Stacks, Instructions) {
-    let mut stacks: Stacks = vec![
-        vec![]; 9];
+    let mut stacks: Stacks = vec![vec![]; 9];
     let mut instructions: Instructions = vec![];
 
     let mut part1 = true;
@@ -43,35 +42,24 @@ fn read_input(s: &str) -> (Stacks, Instructions) {
     (stacks, instructions)
 }
 
-fn part2() {
-    let (mut stacks, instructions) = read_input("inputs/5.txt");
-
-    for instr in instructions {
-        let from = instr.from - 1;
-        let idx = stacks[from].len() - instr.count;
-        for _ in 0..instr.count {
-            let moved = stacks[from].remove(idx);
-            stacks[instr.to - 1].push(moved);
-        }
-    }
-    println!("Part 2: {}", stacks.iter().map(|s| s.last().unwrap()).collect::<String>());
-}
-
-fn part1() {
-    let (mut stacks, instructions) = read_input("inputs/5.txt");
-
-    for instr in instructions {
-        for _ in 0..instr.count {
-            let crt = stacks[instr.from - 1].pop().unwrap();
-            stacks[instr.to - 1].push(crt);
-        }
-    }
-
-    println!("Part 1: {}",
-             stacks.into_iter().map(|s| *s.last().unwrap()).collect::<String>());
-}
-
 fn main() {
-    part1();
-    part2();
+    let (mut stacks1, instructions) = read_input("inputs/5.txt");
+    let mut stacks2 = stacks1.clone();
+
+    for instr in instructions {
+        // Part 1
+        for _ in 0..instr.count {
+            let crt = stacks1[instr.from - 1].pop().unwrap();
+            stacks1[instr.to - 1].push(crt);
+        }
+
+        // Part 2
+        let from = instr.from - 1;
+        let idx = stacks2[from].len() - instr.count;
+        let mut moved = stacks2[from].drain(idx..).collect::<Vec<char>>();
+        stacks2[instr.to-1].append(&mut moved);
+    }
+    println!("Part 1: {}\nPart 2: {}",
+             stacks1.iter().map(|s| s.last().unwrap()).collect::<String>(),
+             stacks2.iter().map(|s| s.last().unwrap()).collect::<String>());
 }
