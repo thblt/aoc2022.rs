fbf32118eeed1efd8f89a88578490b9747fdbf5e
Thibault Polge <thibault@thb.lt>
2022-12-08 09:32:22 +0100

    Day 7 without recursive structures

---
+++ src/day7.rs
@@ -0,0 +1,50 @@
+use lib::*;
+use sscanf::sscanf;
+use std::collections::HashMap;
+
+fn main() {
+    let mut dirs: HashMap<String, u32> = HashMap::new();
+    let mut stack: Vec<String> = vec![];
+    let mut available = 70000000;
+    for line in read_lines("inputs/7.txt") {
+        let line = line.unwrap();
+        if let Ok(cd) = sscanf!(&line, "$ cd {str}") {
+            match cd {
+                ".." => {
+                    stack.pop();
+                }
+                "/" => { /* noop */ }
+                _ => {
+                    stack.push(cd.to_string());
+                }
+            }
+        } else if let Ok(_) = sscanf!(&line, "dir {str}") {
+            let repr = stack.join("/");
+            if !dirs.contains_key(&repr) {
+                dirs.insert(repr, 0);
+            }
+        } else if let Ok((size, _)) = sscanf!(&line, "{u32} {str}") {
+            available -= size;
+            for i in 0..=stack.len() {
+                let key = stack[0..i].to_vec().join("/");
+                let old_size = dirs.get(&key).unwrap_or(&0);
+                dirs.insert(key, size + old_size);
+            }
+        }
+    }
+
+    let need = 30000000 - available;
+    let mut total = 0;
+    let mut best = u32::MAX;
+
+    for (_, size) in dirs {
+        if size <= 100000 {
+            total += size;
+        }
+        if size >= need && size <= best {
+            best = size;
+        }
+    }
+    println!("Part 1: Total size of dirs < 100000 ......................... {:8}", total);
+    println!("Part 2: Size of smallest dir that would free enough space ... {:8}", best);
+}
